26a1c96d9fc4ad16935601e25332fb7685ad496e
Sureshkumar T <tsureshkumar@gmail.com>
2010-01-03 03:43:15 +0530

    simplified further the code. Now it is very elegant.
    - use dispatch json extractors
    - use recursive function for saving the background-1.xml

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,100 +17,73 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
+import dispatch._
+import json._
+import JsHttp._
+
+import dispatch.Http._
+
+import scala.actors.Futures._
+
 import java.io.FileOutputStream
 
 import scala.xml._
-import scala.actors._
-import dispatch._
-import dispatch.liftjson._
-import dispatch.liftjson.Js._
-import net.liftweb.json.JsonAST._
-import scala.util.matching.Regex
-import dispatch.Http._;
 
-case class Photo(id: String, owner: String, secret: String, server: String, farm: Int, title: String, ispublic: Int, isfriend: Int, isfamily: Int) {
-  def url_big = "http://farm%d.static.flickr.com/%s/%s_%s_b.jpg".format(farm, server, id, secret)
-  def url_small = "http://farm%d.static.flickr.com/%s/%s_%s_s.jpg".format(farm, server, id, secret)
-}
-case class Photos(page: Int, pages: Int, perpage: Int, total: Int, photo: List[Photo])
-case class InterestingPhotos(photos: Photos, stat: String)
+object Main {
 
-class Flickr(key:String) {
-  implicit val formats = net.liftweb.json.DefaultFormats // Brings in default date formats etc.
-  def get_intersting_photos() = {
+  def anim_xml (in_folder: String => String, urls :Seq[String]) = {
+    def _anim_xml(prev :String, url :Seq[String]) : Seq[Node] = {
+      url match {
+        case x::y => {
+            (if(prev != null)
+              (<transition><duration>1.0</duration><from>{in_folder(prev)}</from><to>{x}</to></transition>) 
+             else List[Node]()
+           ) ++ (<static><duration>2.0</duration><file>{in_folder(x)}</file></static>) ++ _anim_xml(x,y)
+        }
+        case Nil => List[Node]()
+      }
+    }
+    <background>
+      <starttime> <year>2009</year> <month>08</month> <day>04</day> <hour>00</hour> <minute>00</minute> <second>00</second> </starttime>
+      { _anim_xml(null, urls) }
+    </background>
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val key = args(0)
+    val folder = args(1)
+    val count = Integer.parseInt(args(2))
+    val wait_for_next_sync = Integer.parseInt(args(3))
+
+    def get_file (url: String) = folder + "/" +  url.slice(url.lastIndexOf("/"),url.length)
+
     val url = :/("api.flickr.com") / "services" / "rest" <<? Map(
       "format" -> "json", 
       "method" -> "flickr.interestingness.getList",
-//      "date" -> new java.text.SimpleDateFormat("yyyy-MM-DD").format(new java.util.Date()),
       "nojsoncallback" -> 1,
       "api_key" -> key)
-    (new Http) (url ># { js => 
-      js.extract[InterestingPhotos]
-                      })
-  }
-}
-
-object Main {
-  var key = ""
-  var folder = "/tmp"
-
-  def save_photo(url: String, dir: String) = { 
-    def local_file = dir + "/" +  url.slice(url.lastIndexOf("/"),url.length)
-    (new Http)((url) >>> (new FileOutputStream(local_file)))
-    local_file
-  }
-  
-  def download_photos (count: int) = {
-    var futures = List[Future[Any]]()
-    for(p <- new Flickr(key).get_intersting_photos().photos.photo take count) {
-      futures += Futures.future {save_photo(p.url_big, folder)}
-    }
-    val ret = futures map { f: Future[Any] => f() } 
-    println("download complete")
-    ret.asInstanceOf[List[String]]
-  }
-  
-  def write_animation_xml(files: List[String]) = {
-    val start =  <starttime>
-                    <year>2009</year>
-                        <month>08</month>
-                        <day>04</day>
-                        <hour>00</hour>
-                        <minute>00</minute>
-                        <second>00</second>
-                </starttime>;
-    var xml:List[Node] = List()
-    var prev :String = null
-    for(img <- files) {
-      if(prev != null) {
-        xml ::= (<transition>
-                <duration>5.0</duration>
-                <from>{prev}</from>
-                <to>{img}</to>
-               </transition>)
+      
+    while(true){      
+    
+      val photos = Http(url ># { js => ('photo ! list)(('photos ! obj)(js)) map {
+        v => "http://farm%d.static.flickr.com/%s/%s_%s_b.jpg".format(
+          ('farm ! num)(v).intValue,
+          ('server ! str)(v),
+          ('id ! str)(v),
+          ('secret ! str)(v)
+        )
+      }}) take count
+    
+      while(photos.size > 0) {
+        val first_10  = photos take 10
+        // download photos parallelly
+        val futures = first_10 map { url => future { Http((url) >>> (new FileOutputStream(get_file(url))) ) } }
+        XML.saveFull(folder + "/background-1.xml", anim_xml(get_file,first_10),"UTF-8", true, null)
+        futures map { _() } // wait for download complete
+        println("download completed")
+        photos drop 10
+        Thread.sleep(wait_for_next_sync)
       }
-      xml ::=  <static>
-                  <duration>1795.0</duration>
-                  <file>{img}</file>
-                 </static>;
-      prev = img
-    }
-    <background>{start}{xml}</background>
-  }
-
-  def main(args: Array[String]) = {
-    key = args(0)
-    folder = args(1)
-    val each_time = Integer.parseInt(args(2))
-    val sync_every = Integer.parseInt(args(3))
-    var cond = true
-    while(cond) {
-      val downloaded = download_photos(each_time)
-      //val downloaded = List("abcd", "cdef", "ijkl")
-      XML.saveFull(folder + "/background-1.xml", write_animation_xml(downloaded),"UTF-8", true, null)
-      Thread.sleep(sync_every)
-      //cond = false
     }
   }
-
 }
